40fd490bc37549c328822a7c4271600ec0219b0b
= <alexander.alegre@outlook.com>
2017-06-08 14:17:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let app = express();
+
+
+// set port for heroku deployment
+const PORT = process.env.PORT || 3000;
+// static files
+app.use(express.static('public'))
+// set templating engine
+app.set('view engine', 'pug');
+// set views folder
+app.set('views', './views');
+
+// GET /
+app.get('/', function(req, res) {
+    res.render('home', { title: 'FCC Weather App' });
+});
+
+// listen on port 3000 or heroku port
+app.listen(PORT, function() {
+    console.log(`http://localhost:${PORT}`);
+})
